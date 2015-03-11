0c2596d68c5493abf685ed01c1bdc6ee6e2078f7
ltmartin <ltmartin@uci.cu>
2015-03-11 10:21:35 -0400

    J-ISIS source code

---
+++ FreeTextSearch/build/classes-generated/org/unesco/jisis/freetextsearch/Bundle.java
@@ -0,0 +1,34 @@
+package org.unesco.jisis.freetextsearch;
+/** Localizable strings for {@link org.unesco.jisis.freetextsearch}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>FreeSearch</i>
+     * @see FreeSearchTopComponent
+     */
+    static String CTL_FreeSearchAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_FreeSearchAction");
+    }
+    /**
+     * @return <i>Free Text Search</i>
+     * @see FreeSearchTopComponent
+     */
+    static String CTL_FreeSearchTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_FreeSearchTopComponent");
+    }
+    /**
+     * @return <i>Free Text Search</i>
+     * @see FreeTextSearchAction
+     */
+    static String CTL_FreeTextSearchAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_FreeTextSearchAction");
+    }
+    /**
+     * @return <i>This is a FreeSearch window</i>
+     * @see FreeSearchTopComponent
+     */
+    static String HINT_FreeSearchTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_FreeSearchTopComponent");
+    }
+    private void Bundle() {}
+}
---
+++ GlobalOperations/build/classes-generated/org/unesco/jisis/global/Bundle.java
@@ -0,0 +1,34 @@
+package org.unesco.jisis.global;
+/** Localizable strings for {@link org.unesco.jisis.global}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Global Action</i>
+     * @see GlobalAction
+     */
+    static String CTL_GlobalAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_GlobalAction");
+    }
+    /**
+     * @return <i>GlobalOperations</i>
+     * @see GlobalOperationsTopComponent
+     */
+    static String CTL_GlobalOperationsAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_GlobalOperationsAction");
+    }
+    /**
+     * @return <i>GlobalOperations Window</i>
+     * @see GlobalOperationsTopComponent
+     */
+    static String CTL_GlobalOperationsTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_GlobalOperationsTopComponent");
+    }
+    /**
+     * @return <i>This is a GlobalOperations window</i>
+     * @see GlobalOperationsTopComponent
+     */
+    static String HINT_GlobalOperationsTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_GlobalOperationsTopComponent");
+    }
+    private void Bundle() {}
+}
